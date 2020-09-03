1f6415e41f4683a4accd7d44a69ec7d626f2fca3
vanvan <vanvan@Sarahui-iMac.local>
2020-09-03 17:01:50 +0900

    Initial commit

---
+++ src/views/About.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+  </div>
+</template>
+<script lang="ts">
+import {Component, Vue} from "vue-property-decorator";
+import HelloWorld from "../components/HelloWorld.vue";
+
+@Component({
+  components: {
+    HelloWorld
+  }
+})
+export default class About extends Vue {}
+</script>
